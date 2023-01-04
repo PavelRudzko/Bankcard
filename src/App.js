b9acc08f76228f3903625541ea1ddb22ff51bdfc
Pavel Rudzko <est1@inbox.ru>
2023-01-04 23:35:29 +0100

    checkCVC

--- src/App.js
+++ src/App.js
@@ -4,130 +4,156 @@ import './App.css';
 //import FrontComponent from './FrontComponent'
 
 function App() {
- const[name,setName] = useState("") ;
- const[cardNumber,setCardnumber] = useState('');
- const [month,setMonth] = useState("") ;
- const [monthError, setMonthError] = useState(false) ;
-const [year,setYear] = useState("") ;
-const [cvcNumber,setCvcNumber] = useState() ;
+  const [name, setName] = useState("");
+  const [cardNumber, setCardnumber] = useState('');
+  const [month, setMonth] = useState("");
+  const [monthError, setMonthError] = useState(false);
+  
+  const [year, setYear] = useState("");
+  const [cvcNumber, setCvcNumber] = useState();
+  const [cvcError, setCvcError] = useState(false);
+  
 
   return (
-    <div className = 'ccMain'>  
+    <div className='ccMain'>
 
-<div className = " ccLeftSide">
+      <div className=" ccLeftSide">
 
-<div className = "ccFrontSide">
+        <div className="ccFrontSide">
 
-  <div className = "ccFrontName">      
-    {name|| "Pavel R" }    
-     </div>
+          <div className="ccFrontName">
+            {name || "Pavel R"}
+          </div>
 
-  <div className = "ccFrontNumber"> 
-  {cardNumber || "1111 2222 3333 4444" } 
-  </div>
-  
-  <div className = "ccFrontDate">    
-   {month || "00"}/{ year || "00"}     
-  </div>
+          <div className="ccFrontNumber">
+            {cardNumber || "1111 2222 3333 4444"}
+          </div>
 
-</div>
-                  
-<div className ="ccBackSide">
-  <div className = "ccBackCvc">   
-     { cvcNumber||"000"}    
-       </div>
-  </div>
+          <div className="ccFrontDate">
+            {month || "00"}/{year || "00"}
+          </div>
 
-</div>
+        </div>
+
+        <div className="ccBackSide">
+          <div className="ccBackCvc">
+            {cvcNumber || "000"}
+          </div>
+        </div>
+
+      </div>
 
 
-<div className = "ccRightSide">
+      <div className="ccRightSide">
 
-  <form className ="ccForm">
+        <form className="ccForm">
 
-  <div className = "ccInputRow">
-    <label>
-     Card holdername   
-    </label> 
-    <input placeholder ="e.g Pavel" value ={name}
-    onChange ={(e)=>setName(e.target.value)}/>
-  </div>
+          <div className="ccInputRow">
+            <label>
+              Card holdername
+            </label>
+            <input placeholder="e.g Pavel" value={name}
+              onChange={(e) => setName(e.target.value)} />
+          </div>
 
- <div className = "ccInputRow">
-    <label>
-     Card number
-    </label>
-    <input
-      value={cardNumber}
-      placeholder="e.g. 1234 5678 9012 0000"
-      onChange ={(e)=> {
-        const parts = e.target.value.replaceAll(/\s/g, '').match(/[0-9]{1,4}/g);
-        if (parts) {
-          const [a, b, c, d] = parts;
-          console.log(a, b ,c ,d)
-          setCardnumber([a, b, c, d].filter(Boolean).join(' '))
+          <div className="ccInputRow">
+            <label>
+              Card number
+            </label>
+            <input
+              value={cardNumber}
+              placeholder="e.g. 1234 5678 9012 0000"
+              onChange={(e) => {
+                const parts = e.target.value.replaceAll(/\s/g, '').match(/[0-9]{1,4}/g);
+                if (parts) {
+                  const [a, b, c, d] = parts;
+                  console.log(a, b, c, d)
+                  setCardnumber([a, b, c, d].filter(Boolean).join(' '))
+                } else {
+                  setCardnumber('Please enter the number');
+                }
+              }}
+            />
+          </div>
+
+          <div>
+            <div className="ccInputDateGroup">
+              <label>
+                Expiration date (MM/YY)
+              </label>
+              <div className="ccDateInputs">
+                <input
+                  value={month}
+                  placeholder="MM"
+                  //type= "number"
+                  onChange={(e) => {
+                    const number = Number.parseInt(e.target.value, 10);
+
+                    //alert('YOUR NUMBER: ' + number)
+
+
+                    if (!number) {
+                      setMonth('');
+                    } else if (number < 13) {
+                      setMonth(number);
+                      setMonthError(false);
+                    } else {
+                      setMonthError(true);
+                    }
+
+
+                    // if(e.target.value > 0 && e.target.value < 13 || e.target.value === ''){
+                    //   setMonth(e.target.value);
+                    // }
+                  }}
+                />
+                {monthError && (
+                  <div>Please enter valid month</div>
+                )}
+
+                <input placeholder="YY" maxLength="2"
+                  //type= "number"
+                  onChange={(e) => setYear(e.target.value.match(/^[0-9]*$/))} />
+              </div>
+            </div>
+
+            <div className="ccInputCvcGroup">
+              <label> CVC </label>
+              <input
+                value={cvcNumber}
+                placeholder="123" maxLength="3"
+                
+                onChange={(e) =>{ const cvcNumber = Number.parseInt(e.target.value, 10);//setCvcNumber(e.target.value.match(/^[0-9]*$/))}
+
+                if (!cvcNumber) {
+                setCvcNumber('');
+        } else if (cvcNumber < 1000) {
+                setCvcNumber(cvcNumber);
+              setCvcError(false);
         } else {
-          setCardnumber('');
-        }
+                setCvcError(true);
+        }        
+       
       }}
     />
-  </div>
+              {cvcError && (
+                <div>Can'tbeblank</div>
+              )}
 
-<div>  
-    <div className ="ccInputDateGroup">
-    <label>
-     Expiration date (MM/YY)
-    </label>
-     <div className = "ccDateInputs">     
-    <input
-      value={month}
-      placeholder="MM"
-      type= "number"
-      onChange ={(e)=>{
-        const number = Number.parseInt(e.target.value, 10);
 
-        //alert('YOUR NUMBER: ' + number)
 
-        if (!number) {
-          setMonth('');
-        } else if (number < 13) {
-          setMonth(number);
-          setMonthError(false);
-        } else {
-          setMonthError(true);
-        }
+            </div>
 
-        
-        // if(e.target.value > 0 && e.target.value < 13 || e.target.value === ''){
-        //   setMonth(e.target.value);
-        // }
-      }}
-    />
-    {monthError && (
-      <div>Please enter valid month</div>
-    )}
+          </div>
 
-    <input placeholder="YY"maxLength="2"
-     onChange ={(e)=>setYear(e.target.value)}/>
+          <div className="ccFormButton">
+            <button>Confirm</button>
+          </div>
+        </form>
       </div>
     </div>
 
-  <div className = "ccInputCvcGroup">
-    <label> CVC </label>
-    <input placeholder ="123" maxLength="3" 
-     onChange ={(e)=>setCvcNumber(e.target.value.match(/^[0-9]*$/))}/>    
-  </div>
 
-</div>
-
-  <div className="ccFormButton"> 
-    <button>Confirm</button>
-  </div>
-</form>
-</div>  
-</div>
-
-   
   );
 }
 
